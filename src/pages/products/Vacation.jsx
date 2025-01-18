d7fa0e3464eadf949c6d514a9d8e47a55ffa0c63
Dvooskid <dvooskid1234@gmail.com>
2025-01-18 21:06:49 +0100

    Add files via upload

--- src/pages/products/Vacation.jsx
+++ src/pages/products/Vacation.jsx
@@ -5,33 +5,33 @@ import "./Vacation.css";
 const Vacation = () => {
   return (
     <div className="container-fluid px-0 bg-dark m-0">
-      <div className="vacation d-flex flex-column justify-content-end align-items-center ">
-        <h1 className="fw-bold display-4 red-text black-text-shadow mb-5">
+      <section className="vacation d-flex flex-column justify-content-end align-items-center position-relative">
+        <h1 className="fw-bold display-1 red-text black-text-shadow mb-4">
           Vacation (Tours & Special packages)
         </h1>
-      </div>
+      </section>
 
-      <div className="row">
-        <div className="d-flex justify-content-center align-items-center p-5">
-          <div className="col-md-6 bg-black me-4 rounded white-shadow p-5">
-            <p className="lead text-white">
+      <section className="row mx-0">
+        <div className="d-flex flex-column flex-md-row justify-content-center align-items-center p-4 p-md-5 gap-4">
+          <div className="col-12 col-md-6 bg-black rounded white-shadow p-4 p-md-5">
+            <p className="lead text-white mb-0">
               Escape the ordinary and explore the extraordinary with our
-              tailored vacation packages. Whether it’s a serene getaway, a
+              tailored vacation packages. Whether it's a serene getaway, a
               family adventure, or a romantic retreat, our tours and special
               offers promise unforgettable moments and cherished memories. Your
               dream vacation starts here!
             </p>
           </div>
-          <div className="col-md-6 img-thumbnail gray-shadow">
+          <div className="col-12 col-md-6 img-thumbnail gray-shadow">
             <img
-              id="img"
               className="img-fluid"
               src={corporateImage}
               alt="Corporate Travel"
+              loading="lazy"
             />
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
